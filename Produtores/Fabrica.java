65403474e2849a70e06c8f0ffcdb0081f5fdbd45
Lucas Guimaraes Moreira <103656598+JkaiPrime@users.noreply.github.com>
2024-05-01 21:16:08 -0300

    Que bosta

--- Produtores/Fabrica.java
+++ Produtores/Fabrica.java
@@ -1,27 +1,28 @@
 package Produtores;
 
 public class Fabrica {
-    Ferramentas[] ferramentas = new Ferramentas[5];
-    Sinaleiro[] sinaleiro = new Sinaleiro[4];
-        
-        for (int i = 0; i < 5; i++) {
-            ferramentas[i] = new Ferramentas();
-        }
+    public void produzir(){
+        Ferramentas[] ferramentas = new Ferramentas[5];
+        Sinaleiro[] sinaleiro = new Sinaleiro[4];
+            
+            for (int i = 0; i < 5; i++) {
+                ferramentas[i] = new Ferramentas();
+            }
 
-        for (int j = 0; i < 4; i++) {
-            sinaleiro[j] = new Sinaleiro();
-        }
-        
-        Operarios[] operarios = new Operarios[5];
-        for (int k = 0; i < 5; i++) {
-            Ferramentas ferramentaDireita = ferramentas[k];
-            Ferramentas ferramentaEsquerda = ferramentas[(k + 1) % 5];
-            operarios[k] = new Operarios("Josias", ferramentaDireita,sinaleiro[k]) {
-                @Override
-                public void run() {
-                    super.run(); 
-                }
-            };
-        }
-
+            for (int i = 0; i < 4; i++) {
+                sinaleiro[i] = new Sinaleiro();
+            }
+            
+            Operarios[] operarios = new Operarios[5];
+            for (int i = 0; i < 5; i++) {
+                Ferramentas ferramentaDireita = ferramentas[i];
+                //Ferramentas ferramentaEsquerda = ferramentas[(i + 1) % 5];
+                operarios[i] = new Operarios("Josias", ferramentaDireita,sinaleiro[i]) {
+                    @Override
+                    public void run() {
+                        super.run(); 
+                    }
+                };
+            }
+    }
 }
