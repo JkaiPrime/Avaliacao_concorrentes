9b444a5ebaa98c16b7eb1ecc44b524764d21fff7
Lucas Guimaraes Moreira <103656598+JkaiPrime@users.noreply.github.com>
2024-05-07 22:12:05 -0300

    Criado a classe do carro

---
+++ Outros/Carro.java
@@ -0,0 +1,13 @@
+package Outros;
+
+public class Carro {
+    int id;
+    String color;
+    String tipo;
+
+    public Carro(int id, String color, String tipo){
+        this.id = id;
+        this.color = color;
+        this.tipo = tipo;
+    }
+}
